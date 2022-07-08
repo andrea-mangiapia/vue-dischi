9a95d0f28fe2a4ee738190b7cc7df6882c18de81
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-07-08 19:00:37 +0200

    done header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>       
+            <img src="../assets/logo-small.svg" alt="Logo">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/variables.scss';
+
+header {
+    background-color: $brand_primary_color;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    padding-left: 15px;
+
+    img {
+        width: 20px;
+    }
+}
+
+</style>
---
+++ src/components/SectionListDisc.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        Lista Dischi
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SectionListDisc'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/variables.scss';
+
+section {
+    color: $primary_text;
+}
+</style>
